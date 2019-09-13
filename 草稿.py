e6c0e3dac64456ec5629e059cdd5b5153e475b8c
laibaobaby <49736523+laibaobaby@users.noreply.github.com>
2019-09-13 12:07:42 -0700

    Add files via upload

---
+++ 草稿.py
@@ -0,0 +1,16 @@
+def reverse(x):
+    reverse_x = 0
+    abs_x = abs(x)
+    while abs_x!= 0:
+        temp = abs_x%10
+        reverse_x = reverse_x*10 + temp
+        abs_x = abs_x//10
+    if reverse_x < 2147483647:
+        if x > 0:
+            return reverse_x
+        else:
+            return -reverse_x
+    else:
+        return 0
+
+#Since reverse_x can not exceed [-2**31,2**31-1]. So if condition has to be placed after while loop
