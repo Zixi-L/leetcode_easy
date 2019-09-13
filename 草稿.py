fd9cc4f3a70a85e6c5136a765ff9e1a5fee4b583
laibaobaby <49736523+laibaobaby@users.noreply.github.com>
2019-09-13 12:03:45 -0700

    Add files via upload

---
+++ 草稿.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # dummy is a fixed pointer, pointing to the beginning of the list which is ListNode(0)
+        dummy = cur = ListNode(0) 
+        # traversal l1 and l2
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next=l1
+                l1=l1.next
+            else:
+                cur.next=l2
+                l2=l2.next
+            cur=cur.next
+        # if l1=[1,2,3] l2=[5,6,7] then in the while loop only l1 is inserted.
+        # So we have to insert l2 in this case
+        cur.next=l1 or l2
+        return dummy.next
+    
+# Time complexity = O(m+n)
