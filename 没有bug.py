41d3a74490ac4ffca58ba605c1b2136bccc74d53
laibaobaby <49736523+laibaobaby@users.noreply.github.com>
2019-09-17 11:28:34 -0700

    Add files via upload

--- 没有bug.py
+++ 没有bug.py
@@ -1,8 +1,12 @@
-def strStr(haystack, needle):
-    for i in range(len(haystack)-len(needle)+1):
-        if haystack[i:i+len(needle)] == needle:
-            return i-1
-    return -1
+def search(nums, target):
+    if target in nums:
+        return nums.index(target)
+    else:
+        for i in range(len(nums)):
+            if target > nums[-1]:
+                return len(nums)
+            elif target > nums[i] and target < nums[i+1]:
+                return i+1
 
-print(strStr('hello','ll'))
-                   
+
+print(search([1,3,5,6], 0))
