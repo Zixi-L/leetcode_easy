ff08e9fa37cb2b3565c3db6f2d1942eb2e23cfbe
laibaobaby <49736523+laibaobaby@users.noreply.github.com>
2020-04-04 14:49:33 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+nums1 = [0]
+m = 0
+nums2 = [1]  
+n = 1
+
+l1,l2,end = m-1, n-1 , m+n-1
+
+while l1 >= 0 and l2>= 0:
+        if nums2[l2] > nums1[l1]:
+                nums1[end] = nums2[l2]
+                l2 -= 1     
+        
+        else: 
+                nums1[end]=nums1[l1]
+                l1 -= 1
+            
+        end -=1
+        
+if l1<0:
+        nums1[:l2+1] = nums2[:l2+1]
+
+print(nums1)
